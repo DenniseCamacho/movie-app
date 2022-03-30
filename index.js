1a0ecf34d7c8c2aa8a954ed2cf34f84ac253016a
Dennise Camacho <98361785+DenniseCamacho@users.noreply.github.com>
2022-03-29 22:40:30 -0500

    delete button is functioning.

--- index.js
+++ index.js
@@ -3,25 +3,49 @@
  * VARS, ARRAY AND OBJ
  */
 
-//THIS TAKES THE DIV WITH THE ID TO PUT THE INFORMATION IN IT.
+//THIS TAKES THE DIV WITH THE ID NAME & PUTS INFORMATION IN THAT DIV.
 let output_information = document.getElementById('output-information');
 
-//THIS IS THE URL THE INFORMATION IS COMING FROM.
+//URL THE INFORMATION IS COMING FROM.
 const movies_url = 'https://marked-meadow-provelone.glitch.me/movies';
 
-//THIS IS SO I COULD USE TEMPLATE LITERALS, BUT NOT SURE IF IT MAKES ANY DIFFERENCE.
-const willBootStrapWorkStart = `<div class="card my-3 m-auto" style="max-width: 500px;">`
-const cardDiv2 = `<div class="row g-0">`
-const cardDiv3 = `<div class="col-4">`
-const cardDiv4 = `<div class="col-8">`
-const cardDiv5 = `<div class="card-body">`
-const bootStrapCardTitle = `<h5 class="card-title">`
-const theDetailsTag = `<details>`
-const detailsEnd = `</details>`
-const h5End = `</h5>`
-const divEnd = `</div>`
-const brTags = `<br>`
+//FOR TEMPLATE LITERALS, UNSURE IF IT MAKES ANY DIFFERENCE.
+const willBootStrapWorkStart = `<div class="card my-3 m-auto" style="max-width: 500px;">`;
+const cardDiv2 = `<div class="row g-0">`;
+const cardDiv3 = `<div class="col-4">`;
+const cardDiv4 = `<div class="col-8">`;
+const cardDiv5 = `<div class="card-body">`;
+const bootStrapCardTitle = `<h5 class="card-title">`;
+const theDetailsTag = `<details>`;
+const detailsEnd = `</details>`;
+const h5End = `</h5>`;
+const divEnd = `</div>`;
+const brTags = `<br>`;
+const delButton = `<button id="delete-btn">Delete</button>`;
+const editButton = `<button id="edit-btn">Edit</button>`;
 
+//THIS FUNCTION GETS THE API, CALLED AGAIN INSIDE THE DELETE BUTTON.
+function getMovies() {
+fetch(movies_url).then( (response) => {
+    response.json()
+        .then( (movies) => {                                   //   <--FETCH
+            callCards(movies);
+			deleteButton(movies);
+			editButton2(movies);
+    });
+});
+}
+// function getMovies(){
+// 	fetch(movies_url).then( (response) => {
+// 		response.json()
+// 			.then( (movies) => {                                   //   <--FETCH
+// 				callCards(movies);
+// 				deleteButton(movies);
+// 				editButton2(movies);
+// 			});
+// 	});
+//
+// };
 function callCards(movies){
     movies.forEach( (movie , index, a)  => {
         const movie_title    = movie.title   ;
@@ -56,6 +80,7 @@ function callCards(movies){
 							Plot:	 	${movie_plot}     ${brTags}
 							Actors:	 	${movie_actors}   ${brTags}
 					${detailsEnd}
+					${delButton}${editButton}
 					${divEnd}
 					${divEnd}
 					${divEnd}
@@ -63,15 +88,36 @@ function callCards(movies){
 					`
         }
     });
-}
+};
+function deleteButton(movies){
+	console.log(movies)
+	let delButton2 = document.querySelectorAll('#delete-btn');
+	delButton2.forEach((value, key, parent) => {
+		value.addEventListener("click", (e)=>{
+			e.preventDefault();
+			let mid = movies[key].id;
+			fetch(`${movies_url}/${mid}`,{
+				method: "DELETE"
+			}).then((result)=>{
+				result.json().then((resp)=>{
+					// console.log(resp);
+					getMovies(resp);
+				})
+			})
+			console.log(movies[key].id);
+		});
+	});
+};
+function editButton2(movies){
+	let editButton2 = document.querySelectorAll('#edit-btn');
+	editButton2.forEach((value, key, parent) => {
+		value.addEventListener("click", (e)=>{
+			e.preventDefault();
+			console.log("you clicked the edit button");
+		});
+	});
+};
 
-
-fetch(movies_url).then( (response) => {
-    response.json()
-        .then( (movies) => {                                   //   <--FETCH
-            callCards(movies);
-    });
-});
-
+getMovies();
 // output_information.innerHTML += movies_url;
 
